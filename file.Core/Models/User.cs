34f1061c1d92932d6b576ce0c1814f1ac29a3ed5
Moises Piñate <moises.pinate@outlook.com>
2020-02-27 15:18:44 -0400

    Added user entity

---
+++ file.Core/Models/User.cs
@@ -0,0 +1,10 @@
+namespace file.Core.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public byte[] picture { get; set; }
+    }
+}
