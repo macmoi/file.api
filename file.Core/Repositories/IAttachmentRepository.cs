2d0ee3c331f4232a9892b1fcaf11cf5af22e4ee9
Moises Piñate <moises.pinate@outlook.com>
2020-02-27 20:59:30 -0400

    Added missing usings

--- file.Core/Repositories/IAttachmentRepository.cs
+++ file.Core/Repositories/IAttachmentRepository.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using file.Core.Models;
+
 namespace file.Core.Repositories
 {
     public interface IAttachmentRepository : IRepository<Attachment>
