7c0e24acc432e7b4cf6cd37ae1d60d01e70c2c8d
Moises Piñate <moises,pinate@outlook.com>
2020-02-28 21:54:55 -0400

    Added user resources for mapping out API responses

---
+++ file.Api/Resources/UserResource.cs
@@ -0,0 +1,9 @@
+namespace file.Api.Resources
+{
+    public class UserResource
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+    }
+}
