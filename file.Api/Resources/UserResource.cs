dcadf2da5ce781ec479aa17189dac87c66cd652c
Moises Piñate <moises,pinate@outlook.com>
2020-02-29 02:46:19 -0400

    Added validation annotations

--- file.Api/Resources/UserResource.cs
+++ file.Api/Resources/UserResource.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace file.Api.Resources
 {
     public class UserResource
     {
+        [Required]
         public int id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string name { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string lastName { get; set; }
+        public IList<AttachmentResource> attachmentResources { get; set; }
     }
 }
